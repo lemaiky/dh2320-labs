d3269c827889336c30f5083e5342461f7413e262
Mai-Khanh LE <lemaikhanh.lmk@gmail.com>
2017-02-23 11:21:10 +0100

    Task 5.2 + Bonus

--- Assignment05/code/exercises/task/task.h
+++ Assignment05/code/exercises/task/task.h
@@ -38,7 +38,10 @@ void DefineBallisticParabolaControlPolygon(const ogl::Vec3f& ShootingTankPos, co
     //TODO: Define the control polygon so that the the projectile will move from the
     //shooting tank to the target following a parabola
     ControlPolygon.push_back(ShootingTankPos);
-    ControlPolygon.push_back(ogl::Vec3f(0,0,5));
+    ControlPolygon.push_back(ogl::Vec3f(0, 0, 5));
+    ControlPolygon.push_back(ogl::Vec3f(0, 10, 5));
+    ControlPolygon.push_back(ogl::Vec3f(10, 0, 5));
+    ControlPolygon.push_back(ogl::Vec3f(20, 20, 5));
     ControlPolygon.push_back(TargetTankPos);
 }
 
@@ -54,16 +57,33 @@ void Bezier(const std::vector< ogl::Vec3f >& ControlPolygon, const int NumPoints
     //For the bonus task 5.3, make this work for an arbitrary number of control points
     //Here, just a simple thing, not a Bezier Curve
 
+    int noOfIterations = ControlPolygon.size() - 1;
+
     std::vector< ogl::Vec3f > tempPolygon;
+    std::vector< ogl::Vec3f > intermediatePoints;
     std::copy(ControlPolygon.begin(), ControlPolygon.end(), std::back_inserter(tempPolygon));
 
     for(int i(0);i<NumPoints;i++)
     {
         const float t = float(i) / float(NumPoints-1);
-        
-        BezierCurve[i] = float(pow(1-t, 2)) * tempPolygon[0] + 2 * t * (1-t) * tempPolygon[1] + float(pow(t, 2)) * tempPolygon[2];
-        //BezierCurve[i] = (1-t) * ControlPolygon.front() + t * ControlPolygon.back();
 
+        for(int j(0); j < (int)noOfIterations; j++) {
+            for( int k(0); k < (int)tempPolygon.size(); k++ ) {
+                const ogl::Vec3f& LeftPoint = tempPolygon[k];
+                const ogl::Vec3f& RightPoint = tempPolygon[(k+1) % tempPolygon.size()];
+
+                intermediatePoints.push_back((1-t) * LeftPoint + t * RightPoint);
+            }
+            tempPolygon.clear();
+            std::copy(intermediatePoints.begin(), intermediatePoints.end(), std::back_inserter(tempPolygon));
+            intermediatePoints.clear();
+        }
+        BezierCurve[i] = tempPolygon.front();
         BezierCurve[i][2] = 5 - pow(4*(t-0.5f), 2); //Give it some arbitrary height
+
+        tempPolygon.clear();
+        std::copy(ControlPolygon.begin(), ControlPolygon.end(), std::back_inserter(tempPolygon));
+
+        //BezierCurve[i] = (1-t) * ControlPolygon.front() + t * ControlPolygon.back();
     }
 }
